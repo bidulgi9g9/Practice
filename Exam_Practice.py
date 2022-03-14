dc194ca2c33992dd23172a9d8f5cfed694f94831
LeeMinHyuk <hl2duu2@naver.com>
2022-03-15 00:44:50 +0900

    Exame Practice1

---
+++ Exam_Practice.py
@@ -0,0 +1,20 @@
+from ast import While
+from tracemalloc import stop
+
+
+print("파이썬 중간고사 제 이름은 이민혁 학번은 0000000입니다.")
+
+inkjet = [100, 20]
+laserjet = [200, 50]
+money = 200000
+
+Order = int(input("명령을 입력하세요(1=사용 2=상태 3=보충 4=종료)"))
+
+while True:
+    if Order >= 5:
+        stop
+    elif Order == 4:
+
+
+
+  
