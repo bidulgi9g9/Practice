2252b008e869878b3556db5b666899a53ea8cb57
LeeMinHyuk <hl2duu2@naver.com>
2022-03-17 22:36:14 +0900

    practice3

---
+++ print(abs(-5)).py
@@ -0,0 +1,11 @@
+print(abs(-5)) #절댓값
+print(pow(4, 2)) #4의2승
+print(max(5, 12)) #둘중 큰값을 출력
+print(min(5, 12)) #둘중 작은 값을 출력
+print(round(3.14)) #반올림한 값을 출력
+print(round(4.99)) #반올림한 값을 출력
+
+from math import * #math라이브러리 안의 모든것을 사용
+print(floor(4.99)) #내림한 값을 출력
+print(ceil(3.14)) #올림한 값을 출력
+print(sqrt(16)) #제곱근을 출력
