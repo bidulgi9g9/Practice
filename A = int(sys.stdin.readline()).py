53958dbcd0902233c703d03bf180ce863772087b
LeeMinHyuk <hl2duu2@Naver.com>
2022-05-10 12:56:47 +0900

    9jucha

--- A = int(sys.stdin.readline()).py
+++ A = int(sys.stdin.readline()).py
@@ -1,4 +1,13 @@
-A = int(sys.stdin.readline())
-B = int(sys.stdin.readline())
+coins = ['BTC':50000, 'XRP':20000, 'ADA':30000]
+print("가상화폐 프로그램에 오신 것을 환영합니다")
+print("=" * 15)
 
-print(A + B)
+while True :
+    num = input("명령을 입력하세요(1=입력, 2=내역확인, 3=종료):")
+    if num == 1 :
+        
+    
+    
+
+
+    
